4fb8228434b72f1fd122cb68e0be7b0d6d032ac1
vishnuabhrapudi <58915530+vishnuabhrapudi@users.noreply.github.com>
2019-12-20 20:33:07 +0530

    Update deploy.configuration.cmd

--- deploy.configuration.cmd
+++ deploy.configuration.cmd
@@ -58,8 +58,8 @@ IF DEFINED CLEAN_LOCAL_DEPLOYMENT_TEMP (
 )
 
 IF DEFINED MSBUILD_PATH goto MsbuildPathDefined
-SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
-:MsbuildPathDefined
+SET MSBUILD_PATH=%MSBUILD_15_DIR%\MSBuild.exe
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
